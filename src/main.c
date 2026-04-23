8efa0166e943d69e39912d13697dc27fb74cff47
Erick Yu Xiang Li <lierick08@gmail.com>
2026-04-22 22:14:57 -0300

    inserindo a primeira nota

--- src/main.c
+++ src/main.c
@@ -5,5 +5,8 @@ int main() {
     printf("          sistema Equipe 1         \n");
     printf("===================================\n");
     
+    int nota1 = 0;
+     printf("digite a sua primeira nota: \n");
+     scanf("%d", &nota1);
     
 }
