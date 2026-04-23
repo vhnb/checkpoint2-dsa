6069a99560a7efe0b5b4459f23474e35b0da4a62
vhnb <victorredin122@gmail.com>
2026-04-22 22:01:35 -0300

    Verificação de nota corrigida, para nao retornar resultado invertido

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -11,7 +11,7 @@ int main() {
 
     media = (nota1 + nota2) / 2;
 
-    if (media = 6) {
+    if (media >= 6) {
         printf("Aluno aprovado\n");
     } else {
         printf("Aluno reprovado\n");
