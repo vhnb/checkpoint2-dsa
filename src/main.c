a0c42da7696f35f385cb553f25e9a6d7c708a1d5
ErickBanhos99 <erick.bcastro99@gmail.com>
2026-04-22 22:34:22 -0300

    Criado o caso 1 até o 3.

--- src/main.c
+++ src/main.c
@@ -9,12 +9,36 @@ int main() {
     printf("Digite um número entre 1 a 6\n");
     scanf("%d", valor);
 
-    float nota1 = 0;
+    switch (valor)
+    {
+
+    case 1:
+        float nota1 = 0;
      printf("digite a sua primeira nota: \n");
      scanf("%f", &nota1);
 
          float nota2 = 0;
      printf("digite a sua segunda nota: \n");
      scanf("%f", &nota2);
+        break;
     
+    case 2:
+
+        float media = (nota1 + nota2) / 2;  
+        break;
+    
+    case 3:
+
+        if (media >= 6) {
+        printf("Aluno aprovado\n");
+    } else {
+        printf("Aluno reprovado\n");
+    }
+    
+    break;
+    
+    default:
+        break;
+    }
+
 }
