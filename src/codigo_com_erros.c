0d1498b720db0590f9b4e817199cdfca2f4aeef8
vhnb <victorredin122@gmail.com>
2026-04-22 22:00:35 -0300

    Operadores em parenteres para nao alterar o resultado

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -9,7 +9,7 @@ int main() {
     printf("Digite a segunda nota: ");
     scanf("%f", &nota2);
 
-    media = nota1 + nota2 / 2;
+    media = (nota1 + nota2) / 2;
 
     if (media = 6) {
         printf("Aluno aprovado\n");
